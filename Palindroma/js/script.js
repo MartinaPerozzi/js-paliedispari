af2440fce4b22c2e9231f1c1f404d1b2b1fa623f
MartinaPerozzi <perozzimartina@gmail.com>
2023-01-31 17:52:47 +0100

    Snack1

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -2,17 +2,18 @@
 const buttonSubmit = document.querySelector(".submit-button");
 let userText = document.querySelector(".text");
 
-
+// Creo una funzione che ha come scopo quello di attraversare la parola inserita dall'utente dall'ultima lettera in su.
 function reverseWord(palindrom) {
-
+    // Creo una variabile dove pushare le lettere controllate, otterrò la parola dell'utente al contrario.
     let newString = "";
-
+    // cicla per la lunghezza della parola
     for (let i = palindrom.length - 1; i >= 0; i--) {
         newString += palindrom[i];
-        return newString;
     }
-}
+    // Quando HAI FINITO "ritornami" il risultato.
+    return newString;
 
+}
 
 
 // Al click prendo i dati inseriti dall'utente;
@@ -22,12 +23,16 @@ buttonSubmit.addEventListener(
         // Valori utente
         userText = document.querySelector(".text").value;
 
+        // Avvio la funzione
         const resultOfTest = reverseWord(userText);
 
-        if (resultOfTest == userText) {
-            let message = document.querySelector(".mess").innerHTML += " Palindromo"
-        }
+        // Possibili casi e messaggi per l'utente.
 
+        if (resultOfTest == userText) {
+            let message = document.querySelector(".mess").innerHTML += `La tua parola "${userText}" è un palindromo`
+        } else {
+            message = document.querySelector(".mess").innerHTML += `La tua parola "${userText}" non è un palindromo`;
+        }
     }
 )
 
